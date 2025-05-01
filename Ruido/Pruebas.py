59aa7f94c5aedc67e4232d545f0064f278047d69
FraustoEmi <e.fraustoortiz18@gmail.com>
2025-05-01 17:01:20 -0600

    Creacion de imagenes
    Se cambio la imagen a escala de grises y se le agrego ruido

--- Ruido/Pruebas.py
+++ Ruido/Pruebas.py
@@ -4,6 +4,25 @@ import matplotlib.pyplot as plt
 from skimage import io, color, util
 from scipy.ndimage import gaussian_filter
 
-img = io.imread('Ruido/imagenes/nutria.jpg')
-plt.imshow(img, cmap='gray')
+imagen = io.imread('Ruido/imagenes/nutria.jpg')
+plt.imshow(imagen, cmap='gray')
+
+#Esta imagen tiene 3 canales (RGB), por lo que es conveniente convertirla a escala de grises
+#para evitar problemas de compatibilidad con las funciones de ruido
+
+imagen_grises = color.rgb2gray(imagen)
+plt.imshow(imagen_grises, cmap='gray')
+
+#plt.imsave('Ruido/imagenes/nutria_grises.jpg', imagen_grises, cmap='gray')
+
+#Agregamos ruido gaussiano a la imagen
+imagen_ruido = util.random_noise(imagen_grises, mode='gaussian', var=0.5)
+plt.imshow(imagen_ruido, cmap='gray')
 plt.show()
+
+#plt.imsave('Ruido/imagenes/nutria_ruido.jpg', imagen_ruido, cmap='gray')
+
+MSE = np.mean((imagen_grises - imagen_ruido) ** 2)
+print("MSE:", MSE)
+
+# SE TIENE QUE REDUCIR EL RUIDO DE LA IMAGEN
